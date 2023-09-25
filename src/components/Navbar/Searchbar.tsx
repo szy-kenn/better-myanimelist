885bceec27fb3ac8062a1268466540791d43da93
szy-kenn <johnkennethvelano@gmail.com>
2023-09-25 13:19:03 +0800

    fixed navbar layout

--- src/components/Navbar/Searchbar.tsx
+++ src/components/Navbar/Searchbar.tsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+"use client";
+import React, { useRef, useState } from "react";
+import "@/sass/searchbar.scss";
+import { BiSearch } from "react-icons/bi";
 const Searchbar = () => {
-    return <div className="searchbar">Searchbar</div>;
+    const [searchText, setSearchText] = useState("");
+    const searchRef = useRef<HTMLDivElement>(null);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value);
+    };
+
+    const handleFocus = () => {
+        searchRef.current?.classList.remove("blurred");
+        searchRef.current?.classList.add("focused");
+    };
+
+    const handleBlur = () => {
+        searchRef.current?.classList.remove("focused");
+        searchRef.current?.classList.add("blurred");
+    };
+
+    return (
+        <div className="searchbar">
+            <div ref={searchRef} className="blurred">
+                <input
+                    type="text"
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
+                    value={searchText}
+                />
+                <BiSearch />
+            </div>
+        </div>
+    );
 };
 
 export default Searchbar;
