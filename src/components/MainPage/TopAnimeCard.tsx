25eaf4192934d8e4db415015cc0c37c61978808d
szy-kenn <johnkennethvelano@gmail.com>
2023-10-09 15:28:39 +0800

    added top anime card

--- src/components/MainPage/TopAnimeCard.tsx
+++ src/components/MainPage/TopAnimeCard.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "@/sass/components/top-anime-card.scss";
 
-const TopAnimeCard = () => {
-    return <div>TopAnimeCard</div>;
+const TopAnimeCard = ({ rank, animeId }: { rank: number; animeId: string }) => {
+    return (
+        <div className="top-anime-card">
+            <h1>{rank}</h1>
+            <div className="top-anime-content">
+                <img src="./img/steins-gate.jpg" alt={`Top ${rank} Anime`} />
+                <div className="top-anime-info">
+                    <h3 className="top-anime-title">Steins;Gate</h3>
+                    <div className="top-anime-info-subsection">
+                        <div className="top-anime-type">
+                            <p>TV</p>
+                            <span>24 eps</span>
+                        </div>
+                        <div className="top-anime-status">
+                            <p>Spring 2011</p>
+                            <span>Finished</span>
+                        </div>
+                    </div>
+                </div>
+                <p className="score">9.10</p>
+            </div>
+        </div>
+    );
 };
 
 export default TopAnimeCard;
--- src/components/MainPage/TopAnimeContainer.tsx
+++ src/components/MainPage/TopAnimeContainer.tsx
@@ -1,19 +1,15 @@
 import React from "react";
 import TopAnimeCard from "./TopAnimeCard";
+import "@/sass/components/top-anime-container.scss";
 
 const TopAnimeContainer = () => {
     return (
         <div className="top-anime-container">
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
-            <TopAnimeCard />
+            <TopAnimeCard rank={1} animeId="1" />
+            <TopAnimeCard rank={2} animeId="1" />
+            <TopAnimeCard rank={3} animeId="1" />
+            <TopAnimeCard rank={4} animeId="1" />
+            <TopAnimeCard rank={5} animeId="1" />
         </div>
     );
 };
